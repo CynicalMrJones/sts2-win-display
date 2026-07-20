32addaab2910e84a90a96715d24a0b1375bf712c
juicy <wopjr1234@gmail.com>
2026-07-20 16:05:07 -0400

    Comment out script

--- stripper.py
+++ stripper.py
@@ -1,28 +1,29 @@
-import os
+# import os
 
-p = r"cards/event"
-
-banlist = ["banner.png", "description_text.png",
-           "energy_icon.png",
-           "energy_label.png",
-           "frame.png",
-           "metadata.json",
-           "portrait_border.png",
-           "portrait_native.png",
-           "portrait.png",
-           "prolong.png",
-           "title_text.png",
-           "type_label.png",
-           "type_plaque.png"]
-
-for e in os.scandir(p):
-    card_name = e.name
-    print(f'Card name: {card_name}')
-    path = f'{p}/{card_name}'
-    print(path)
-    for file in os.scandir(path):
-        print(file.name)
-        if file.name == 'composite.png':
-            os.rename(f'{path}/composite.png', f'{path}/{card_name}.png')
-        if file.name in banlist:
-            os.remove(f'{path}/{file.name}')
+# For getting rid of old data
+# p = r"cards/event"
+#
+# banlist = ["banner.png", "description_text.png",
+#            "energy_icon.png",
+#            "energy_label.png",
+#            "frame.png",
+#            "metadata.json",
+#            "portrait_border.png",
+#            "portrait_native.png",
+#            "portrait.png",
+#            "prolong.png",
+#            "title_text.png",
+#            "type_label.png",
+#            "type_plaque.png"]
+#
+# for e in os.scandir(p):
+#     card_name = e.name
+#     print(f'Card name: {card_name}')
+#     path = f'{p}/{card_name}'
+#     print(path)
+#     for file in os.scandir(path):
+#         print(file.name)
+#         if file.name == 'composite.png':
+#             os.rename(f'{path}/composite.png', f'{path}/{card_name}.png')
+#         if file.name in banlist:
+#             os.remove(f'{path}/{file.name}')
