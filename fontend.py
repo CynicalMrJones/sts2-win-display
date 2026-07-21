d1ef3eee7a8bd618322d28abef6fbcbb4566e8bd
CynicalMrJones <wopjr1234@gmail.com>
2026-07-21 13:50:40 -0400

    Moved files for flask and got a card displayed

--- fontend.py
+++ fontend.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+img_folder = os.path.join('static', 'cards', 'colorless')
+app.config["UPLOAD_FOLDER"] = img_folder
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def img_display():
+    test_pic = os.path.join(app.config["UPLOAD_FOLDER"], "splash.png")
+    print(test_pic)
+    return render_template("index.html", user_pic=test_pic)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
