9634427f2b03e600d36a7c67bc34f44057914f74
juicy <wopjr1234@gmail.com>
2026-07-29 12:47:55 -0400

    Can now print all runs of a single character

--- frontend.py
+++ frontend.py
@@ -5,7 +5,7 @@ from markupsafe import Markup
 
 app = Flask(__name__)
 
-img_folder = os.path.join('static', 'cards', 'defect')
+img_folder = os.path.join('static', 'cards', 'ironclad')
 app.config["UPLOAD_FOLDER"] = img_folder
 
 
@@ -23,24 +23,40 @@ def win():
     return render_template("win.html", deck=deck)
 
 
+def get_wins_arr(character: str):
+    win_list = []
+    win = []
+    file = open(character)
+    for line in file:
+        if line == '\n':
+            win_list.append(win)
+            win = []
+        else:
+            win.append(line.strip())
+    return win_list
+
+
 def winhtml(user_pic):
-    old_list = []
+    big_string = ""
+    win_list = get_wins_arr('ironclad.txt')
     stripped = []
-    with open('defect.txt') as f:
-        for e in f:
-            if e == '\n':
-                break
-            old_list.append(e.lower().strip())
-        # Wow this looks dumb
-        del old_list[0]
-        del old_list[0]
-        for e in old_list:
-            stripped.append(e.replace("card.", ""))
 
-        big_string = ""
+    # Wow this looks dumb
+    for win in win_list:
+        for entry in win:
+            stripped.append(entry.lower())
+        print(stripped)
+        del stripped[0]
+        print(stripped)
+        seed = stripped[0]
+        del stripped[0]
+        print('End')
+        big_string = big_string + f'<h1>Seed: {seed}'
+
         for e in stripped:
-            big_string = big_string + f'<img src="static/cards/{e.split(',')[0]}.png" height="342" width="280">'
+            big_string = big_string + f'<img src="static/cards/{e.split(',')[0].replace('card.', '')}.png" height="342" width="280">'
 
+        stripped = []
     return Markup(big_string)
 
 
