e47f57d4973523090ffbe99dbfad6233ded1c1e4
juicy <wopjr1234@gmail.com>
2026-07-29 09:22:44 -0400

    Cleaned up program

--- frontend.py
+++ frontend.py
@@ -18,32 +18,28 @@ def home():
 def win():
     main()
     # Create deck variable with the html content
-    test_pic = os.path.join(app.config["UPLOAD_FOLDER"], "splash.png")
+    test_pic = os.path.join(app.config["UPLOAD_FOLDER"])
     deck = winhtml(test_pic)
-    return render_template("win.html", user_pic=test_pic, deck=deck)
+    return render_template("win.html", deck=deck)
 
 
 def winhtml(user_pic):
-
-    meme = []
+    old_list = []
     stripped = []
     with open('defect.txt') as f:
         for e in f:
             if e == '\n':
                 break
-            meme.append(e.lower().strip())
+            old_list.append(e.lower().strip())
         # Wow this looks dumb
-        del meme[0]
-        del meme[0]
-        print(meme)
-        for e in meme:
+        del old_list[0]
+        del old_list[0]
+        for e in old_list:
             stripped.append(e.replace("card.", ""))
-        print(stripped)
 
         big_string = ""
         for e in stripped:
-            big_string = big_string + f'<img src="static/cards\
-                    /{e.split(',')[0]}.png" height="342" width="280">'
+            big_string = big_string + f'<img src="static/cards/{e.split(',')[0]}.png" height="342" width="280">'
 
     return Markup(big_string)
 
